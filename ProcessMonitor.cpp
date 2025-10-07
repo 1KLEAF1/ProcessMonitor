608ed98a872e1574a41986af16905ea9b5ee0190
1KLEAF1 <magameddz27@gmail.com>
2025-10-07 21:49:58 +0300

    Добавьте файлы проекта.

---
+++ ProcessMonitor.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <tlhelp32.h>
+#include <string>
+
+int main() {
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (hSnapshot == INVALID_HANDLE_VALUE) {
+		std::cerr << "Error: snapshot not created";
+		return 1;
+	}
+	PROCESSENTRY32 pe32;
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+	if (!Process32First(hSnapshot, &pe32)) {
+		std::cerr << "Error:firts process not get";
+		CloseHandle(hSnapshot);
+		return 1;
+	}
+	do {
+		std::wcout << pe32.th32ProcessID << L"\t| " << pe32.szExeFile << std::endl;
+	} while (Process32Next(hSnapshot, &pe32));
+	CloseHandle(hSnapshot);
+	return 0;
+}
